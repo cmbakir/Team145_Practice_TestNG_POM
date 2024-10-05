677009878be2fae817e7da7d363e28d9532cc1d3
cmbakir <cembakir@hotmail.com>
2024-10-06 01:12:20 +0300

    Practice TestNg GunSonu

---
+++ src/test/java/pages/AkakcePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AkakcePage {
+    public AkakcePage (){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@id='q']")
+    public WebElement aramaKutusu;
+
+    @FindBy(xpath = "(//*[@title='Cep Telefonu'])[1]")
+    public WebElement ceptelefonuSekmesi;
+
+    @FindBy (xpath = "//*[@class='a']")
+    public WebElement markasiYazisi;
+
+    @FindBy (xpath = "(//*[@class='fl-ac'])[3]")
+    public WebElement dahiliHafiza;
+
+    @FindBy (xpath = "//*[@id='c109306']")
+    public WebElement checkBox256gb;
+
+    @FindBy (xpath = "//*[@id='FF_v8']")
+    public WebElement solTaraftakiForm;
+
+    @FindBy (xpath = "//*[@class='pt_v8']")
+    public WebElement urunFiyatlari;
+
+
+
+}
---
+++ src/test/java/tests/AkakceArama.java
@@ -0,0 +1,70 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.AkakcePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class AkakceArama {
+
+    // akakce anasayfaya gidin
+    // arama kutusunun locate edin
+    // arama kutusuna "iPhone 16 Pro Max" yazip arattirin
+    // solda cep telefonu sekmesine tiklayin
+    //soldaki 256 gb checkbox'a tiklayin
+    // arama sonucunda urunlerin bulunmabilirligini test edin
+
+    // arama sonucunda kac urun bulundugunu bir List e kaydedin
+    // kaydedilen urunleri kucukten buyuge siraliyin
+    //en ucuz urunun fiyatini ve websayfasini yazdirin
+    // pencereleri kapatin
+
+
+    @Test
+    public void test01(){
+
+
+        Driver.getDriver().get(ConfigReader.getProperty("akakceUrl"));
+
+        AkakcePage akakcePage = new AkakcePage();
+        akakcePage.aramaKutusu.click();
+        akakcePage.aramaKutusu.sendKeys("iPhone 16 Pro Max" + Keys.ENTER);
+        ReusableMethods.bekle(3);
+        akakcePage.ceptelefonuSekmesi.click();
+        ReusableMethods.bekle(3);
+
+        //Actions actions = new Actions(Driver.getDriver());
+       // actions.sendKeys(Keys.PAGE_DOWN).perform();
+       // actions.sendKeys(Keys.PAGE_DOWN).perform();
+       // actions.sendKeys(Keys.PAGE_DOWN).perform();
+        //actions.sendKeys(Keys.PAGE_DOWN).perform();
+       //actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        //akakcePage.dahiliHafiza.click();
+
+        List<WebElement> urunFiyatlariListesiElementi =
+                Driver.getDriver().findElements(By.xpath("//*[@class='pt_v8']"));
+
+       List<String> stringListe = ReusableMethods.getElementsText(urunFiyatlariListesiElementi);
+       Collections.sort(stringListe);
+        System.out.println(stringListe);
+
+
+
+
+        Driver.quitDriver();
+
+    }
+
+
+}
